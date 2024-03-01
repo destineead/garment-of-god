72b615e73385dc6219c6ba0a80a2e7bb5a95e241
destineead <destineenugent1@gmail.com>
2024-03-01 15:43:09 -0500

    Render product images

--- src/components/ProductListItem/ProductListItem.jsx
+++ src/components/ProductListItem/ProductListItem.jsx
@@ -1,9 +1,9 @@
 export default function ProductListItem({product}) {
   return (
-    <div>
+    <div className="product-list-item">
+      <img src={product.images[0]} alt="" id="product-list-item-img"/>
       <p>{product.name}</p>
       <p>{product.price}</p>
-      <p>{product.images}</p>
     </div>
   )
 }
